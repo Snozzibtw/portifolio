03ca25dc79fbcff15f592fb9a57ddaecd0443b7f
Snozzi <99423719+Snozzibtw@users.noreply.github.com>
2025-04-27 15:21:27 +0200

    Working on the music player

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ document.querySelectorAll('.flag').forEach(flag => {
         flag.classList.toggle('clicked');
     });
 });
+
+function changeSong() {
+    const music = document.getElementById('bg-music');
+    const source = document.getElementById('audioSource');
+    const selector = document.getElementById('songSelector');
+    
+    source.src = selector.value;
+    music.load();
+    music.play();
+}
+
+function pauseMusic() {
+    const music = document.getElementById('bg-music');
+    music.pause();
+}
+
+function stopMusic() {
+    const music = document.getElementById('bg-music');
+    music.pause();            // Pause the music
+    music.currentTime = 0;     // Reset to the start
+}
+  
