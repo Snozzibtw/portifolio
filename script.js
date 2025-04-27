583bd065c14c08b68bde66498b02d48abcbbd676
Snozzi <99423719+Snozzibtw@users.noreply.github.com>
2025-04-27 16:59:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ document.querySelectorAll('.flag').forEach(flag => {
     });
 });
 
+function togglePlayPause() {
+    const music = document.getElementById('bg-music');
+    const playButton = document.getElementById('playPauseButton');
+    const playIcon = playButton.querySelector('.fa-play');
+    const pauseIcon = playButton.querySelector('.fa-pause');
+
+    if (music.paused) {
+        // Play the music
+        music.play();
+        // Change the icon to pause
+        playIcon.style.display = 'none';
+        pauseIcon.style.display = 'inline-block';
+    } else {
+        // Pause the music
+        music.pause();
+        // Change the icon to play
+        playIcon.style.display = 'inline-block';
+        pauseIcon.style.display = 'none';
+    }
+}
+
 function changeSong() {
     const music = document.getElementById('bg-music');
     const source = document.getElementById('audioSource');
@@ -14,14 +35,34 @@ function changeSong() {
     music.play();
 }
 
-function pauseMusic() {
+function skipMusic() {
     const music = document.getElementById('bg-music');
-    music.pause();
+    const songSelector = document.getElementById('songSelector');
+    
+    const currentIndex = songSelector.selectedIndex;
+    const nextIndex = (currentIndex + 1) % songSelector.options.length;
+
+    songSelector.selectedIndex = nextIndex;
+
+    music.src = songSelector.options[nextIndex].value;
+    music.play();
 }
 
-function stopMusic() {
+function playbackMusic() {
     const music = document.getElementById('bg-music');
-    music.pause();            // Pause the music
-    music.currentTime = 0;     // Reset to the start
+    const songSelector = document.getElementById('songSelector');
+
+    const currentIndex = songSelector.selectedIndex;
+
+    if (currentIndex == 0) {
+        console.log("No prevoius songs")
+        return; 
+    }
+
+    const nextIndex = (currentIndex - 1) % songSelector.options.length;
+    
+    songSelector.selectedIndex = nextIndex;
+
+    music.src = songSelector.options[nextIndex].value;
+    music.play();
 }
-  
